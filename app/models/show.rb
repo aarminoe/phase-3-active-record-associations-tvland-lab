c1f070c427a3c013b8220288242cfa7be2f3e317
Aaron Noe <aarminoe@gmail.com>
2022-06-27 17:40:32 -0500

    commit tree 907f8186fbd2b07ed4292cb62602dd2d74204c17

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+   "#{self.actors.first.first_name} #{self.actors.first.last_name}"
+  end
 end
